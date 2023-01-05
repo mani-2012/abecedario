61ca6087e1923ae23e681a7e17e79caff5d408b5
mani-2012 <106404786+mani-2012@users.noreply.github.com>
2023-01-05 17:54:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    cuadrado=createCanvas(400, 400);
+    cuadrado.position(850,170);
+    vidio=createCapture(VIDEO);
+    vidio.size(400,400);
+    vidio.position(250,170);
+}
+function preload(){
+
+}
+function draw(){
+
+}
