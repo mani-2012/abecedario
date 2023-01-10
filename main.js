b4d896e2cccb1a05cdcae2c52520a2a4def102f3
mani-2012 <106404786+mani-2012@users.noreply.github.com>
2023-01-10 13:50:51 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
+manoI=0;
+manoD=0;
+restar=0;
 function setup(){
     cuadrado=createCanvas(400, 400);
     cuadrado.position(850,170);
     vidio=createCapture(VIDEO);
     vidio.size(400,400);
     vidio.position(250,170);
+    poste=ml5.poseNet(vidio,pluma);
+    poste.on('pose',mariposa)
 }
 function preload(){
 
 }
-function draw(){
-
+function pluma(){
+    console.log("YA ESTA LISTO");
 }
+function mariposa(resultado){
+    if (resultado.length>0) {
+        console.log(resultado);
+        manoI=resultado[0].pose.leftWrist.x;
+        manoD=resultado[0].pose.rightWrist.x;
+    }
+    }
